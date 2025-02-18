a6af602ee64a4d6b99adc37691750f8517557bde
noplacetohide <nikhildwivedi007@gmail.com>
2025-02-18 16:37:22 +0530

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
             <div className="mx-auto- px-24 tab-s:px-44 tab-l:px-64 scr-s:px-76 scr-m:px-108 scr-l:px-160 py-[40px]">
                 <div className="flex justify-between items-center">
                     <a href="/">
-                        <img src="src/assets/logo.png" className='w-[208px] h-[40px] scr-m:w-[312px] scr-m:h-[60px]' />
+                        <img src="/assets/logo.png" className='w-[208px] h-[40px] scr-m:w-[312px] scr-m:h-[60px]' />
                     </a>
                     <div className="space-x-6">
                         {/* <button>asd</button> */}
